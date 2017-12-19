cca4f316b248236bfec409ea8702eb33fd24b0d0
Randy Layne <randydavidl78@gmail.com>
2017-12-18 19:28:59 -0500

    fixed typos in test.js

--- test/test.js
+++ test/test.js
@@ -3,15 +3,15 @@ const expect = require('chai').expect;
 describe('whichCentury', () => {
     const whichCentury = require('../challenge.js').whichCentury;
     it('Should return a number', () => {
-        expect(whichCentury).to.be.a('number');
+        expect(whichCentury(312)).to.be.a('number');
     });
     it('should return 1 when a number between 1 and 100 is entered', () => {
         expect(whichCentury(5)).to.equal(1);        
         expect(whichCentury(100)).to.equal(1);        
     });
     it('should return 2 when a number between 201 and 300 is entered', () => {
-        expect(whichCentury(5)).to.equal(2);        
-        expect(whichCentury(100)).to.equal(2);        
+        expect(whichCentury(101)).to.equal(2);        
+        expect(whichCentury(200)).to.equal(2);        
     });
     it('should return 19 when a number between 1801 and 1900 is entered', () => {
         expect(whichCentury(1865)).to.equal(19);        
